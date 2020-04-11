57b0c0fdb16b2d46d725615311b6b8d1f5e0afec
Qi <zyfyus@outlook.com>
2020-04-11 12:28:28 +0800

    rewrite use netEasy translate

--- js/bg.js
+++ js/bg.js
@@ -1,25 +1,96 @@
-chrome.extension.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if (request.greeting == "hello") {
-			var req = new XMLHttpRequest()
-			req.open(
-				"GET",
-				"http://openapi.baidu.com/public/2.0/translate/dict/simple?client_id=e2fjm1o0sb4uTTa1WnNgmmGV" +
-				"&from=" + request.param.from +  "&to=" + request.param.to + "&q=" + request.param.q,
-				true)
-			req.onload = function() {
-				sendMsg(JSON.parse(req.response))
-			}
-			req.send(null)
-			sendResponse({farewell: "bye"})
-		} else if (request.greeting == "farewell") {
-		}
-	}
-)
+// 百度翻译 API
+// http://api.fanyi.baidu.com/doc/21
+// const baiduApiUrl = "https://fanyi-api.baidu.com/api/trans/vip/translate";
+// const baiduAppId = "20181101000228392";
+// const baiduKey = "1R5FL6RE9TDLJazPTere";
+
+// let baiduBuildUrl = request => {
+//   let salt = Math.floor(Math.random() * 10000);
+//   let hashable = baiduAppId + request.param.q + salt + baiduKey;
+//   let sign = md5(hashable);
+//   let params = [
+//     `appid=${baiduAppId}`,
+//     `salt=${salt}`,
+//     `from=${request.param.from}`,
+//     `to=${request.param.to}`,
+//     `q=${request.param.q}`,
+//     `sign=${sign}`,
+//   ].join("&");
+//   var url = `${baiduApiUrl}?${params}`;
+//   console.log(hashable, url, sign);
+//   return url;
+// }
+
+// console.log(md5);
+
+
+function buildYouDaoUrl(request) {
+  // 有道接口
+  var appKey = '41eb757863a2c342';
+  var key = 'hAq4XkNzH54xcUkS4onvd4ofOLqgSWEE';
+  var salt = (new Date).getTime();
+  var curTime = Math.round(new Date().getTime() / 1000);
+  var query = request.param.q;
+  // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+  var from = request.param.from;
+  var to = request.param.to;
+  var str1 = appKey + truncate(query) + salt + curTime + key;
+
+  var sign = sha256(str1);
+  var data = {
+    q: query,
+    appKey: appKey,
+    salt: salt,
+    from: from,
+    to: to,
+    sign: sign,
+    signType: "v3",
+    curtime: curTime,
+  };
+  let params = Object.keys(data).map(key => `${key}=${data[key]}`);
+  return 'http://openapi.youdao.com/api?' + params.join('&');
+}
+
+
+function truncate(q) {
+  var len = q.length;
+  if (len <= 20) return q;
+  return q.substring(0, 10) + len + q.substring(len - 10, len);
+}
+
+chrome.extension.onMessage.addListener(function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.greeting == "hello") {
+    // const url = baiduBuildUrl(request);
+    const url = buildYouDaoUrl(request);
+
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.onload = function () {
+      try {
+        // console.log(JSON.parse(req.response));
+        sendMsg(JSON.parse(req.response));
+      } catch (e) {
+        console.log(e, req.response);
+        request.greeting == "farewell"
+      }
+    };
+    req.send();
+
+    sendResponse({ farewell: "bye" });
+  } else if (request.greeting == "farewell") {
+  }
+});
 
 function sendMsg(msg) {
-	chrome.tabs.getSelected(null, function(tab) {
-		chrome.tabs.sendMessage(tab.id, {greeting: "hello", result: msg}, function(response) {
-		})
-	})	
+  chrome.tabs.getSelected(null, function (tab) {
+    chrome.tabs.sendMessage(
+      tab.id,
+      { greeting: "hello", result: msg },
+      function (response) { }
+    );
+  });
 }
--- js/content.js
+++ js/content.js
@@ -1,120 +1,168 @@
-var send
-var last_string
-var wordPos_x
-var wordPos_y
+let send;
+let last_string;
+let wordPos_x;
+let wordPos_y;
 
-window.addEventListener('click', function(e) {
-	listen(e)
-})
+window.addEventListener('click', listen);
 
-var anchor = document.querySelectorAll("a");
+let anchor = document.querySelectorAll('a');
 
-for (var i = 0; i < anchor.length; i++) {
-	addEvent(anchor[i])
+for (let i = 0; i < anchor.length; i++) {
+  addEvent(anchor[i]);
 }
 
 function addEvent(dom) {
-	dom.addEventListener('mouseleave', function(e) {
-		listen(e)
-	})
+  dom.addEventListener('mouseleave', function (e) {
+    listen(e);
+  });
 }
 
 function listen(e) {
-	var string = window.getSelection().toString()
-	if (string) {
-		clearTimeout(send)
-		var param = {
-			from: "en",
-			to: "zh",
-			q: string.trim().toLowerCase().split(" ")[0]
-		}
-		if (string.match(/[^\x00-\x80]/g)) {
-			param.from = "zh"
-			param.to = "en"
-		}
-		if (last_string !== param.q) {
-			send = setTimeout(function() {sendMsg(param)}, 100)
-			last_string = param.q
-			wordPos_x = e.clientX + window.scrollX
-			wordPos_y = e.clientY + window.scrollY
-			// wordPos_x = e.clientX
-			// wordPos_y = e.clientY
-		}
-	} else {
-		last_string = ""
-		if (document.getElementsByClassName("translate_pop").length) {
-			document.getElementsByClassName("translate_pop")[0].remove()
-		}
-	}
+  let string = window.getSelection().toString();
+  if (string) {
+    clearTimeout(send);
+    let param = {
+      from: 'en',
+      to: 'zh',
+      q: string.trim().toLowerCase().split(' ')[0],
+    };
+    if (string.match(/[^\x00-\x80]/g)) {
+      param.from = 'zh';
+      param.to = 'en';
+    }
+    if (last_string !== param.q) {
+      send = setTimeout(function () {
+        sendMsg(param);
+      }, 100);
+      last_string = param.q;
+      wordPos_x = e.clientX + window.scrollX;
+      wordPos_y = e.clientY + window.scrollY;
+      // wordPos_x = e.clientX
+      // wordPos_y = e.clientY
+    }
+  } else {
+    last_string = '';
+    if (document.getElementsByClassName('translate_pop').length) {
+      document.getElementsByClassName('translate_pop')[0].remove();
+    }
+  }
 }
 
 function sendMsg(param) {
-	chrome.extension.sendMessage({greeting: "hello", param: param}, function(response) {
-	})
+  chrome.extension.sendMessage({ greeting: 'hello', param: param }, function (
+    response
+  ) { });
 }
 
-chrome.extension.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		if (request.greeting == "hello") {
-			handler(request.result)
-			sendResponse({farewell: "bye"})
-		} else if (request.greeting == "farewell") {
-		}
-	}
-)
+chrome.extension.onMessage.addListener(function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.greeting == 'hello') {
+    handler(request.result);
+    sendResponse({ farewell: 'bye' });
+  } else if (request.greeting == 'farewell') {
+  }
+});
 
 function handler(result) {
-	if (document.getElementsByClassName("translate_pop").length) {
-		document.getElementsByClassName("translate_pop")[0].remove()
-	}
-	var win_height = window.innerHeight
-	var win_width = window.innerWidth
-	var x_direct = 0
-	var y_direct = 0
-	if (result.errno === 0 && !(typeof(result.data.word_name) === "undefined")) {
-		var pop = document.createElement("div")
-		var title = document.createElement("h3")
-		pop.appendChild(title)
+  if (!result) {
+    alert('translate result got error!');
+  }
+  try {
+    buildYouDaoPop(result);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-		for (var symbol in result.data.symbols) {
-			var symbol_dom = document.createElement("div")
-			symbol_dom.className = "translate_symbol"
-			var symbol_ph = document.createElement("span")
-			symbol_ph.innerText = result.data.symbols[symbol].ph_zh ||
-					result.data.symbols[symbol].ph_am || result.data.symbols[symbol].ph_en
-					|| ""
-			symbol_ph.className = "translate_ph"
-			symbol_dom.appendChild(symbol_ph)
 
-			var parts = result.data.symbols[symbol].parts
-			var symbol_parts = document.createElement("div")
-			symbol_parts.className = "translate_parts"
+function buildYouDaoPop(result) {
+  let win_height = window.innerHeight;
+  let win_width = window.innerWidth;
+  let x_direct = 0;
+  let y_direct = 0;
 
-			for (var part in parts) {
-				var part_dom = document.createElement("i")
-				part_dom.innerText = parts[part].part
-				part_dom.className = "translate_part"
-				symbol_parts.appendChild(part_dom)
+  // remove pop if exsit
+  if (document.getElementsByClassName('translate_pop').length) {
+    document.getElementsByClassName('translate_pop')[0].remove();
+  }
+  let pop = document.createElement('div');
 
-				var means = parts[part].means
-				var means_dom = document.createElement("ol")
-				means_dom.className = "translate_means"
-				for (var mean in means) {
-					var mean_dom = document.createElement("li")
-					mean_dom.innerText = parseInt(mean) + 1 + ". " + means[mean]
-					means_dom.appendChild(mean_dom)
-				}
-				symbol_parts.appendChild(means_dom)
-			}
-			symbol_dom.appendChild(symbol_parts)
-			pop.appendChild(symbol_dom)
-		}
+  // build title
+  let title = document.createElement('h3');
+  title.innerText = result.translation.join(', ');
+  pop.appendChild(title);
+  pop.className = 'translate_pop';
+  pop.style.top = wordPos_y + 'px';
+  pop.style.left = wordPos_x + 'px';
 
-		title.innerText = result.data.word_name
-		pop.className = "translate_pop"
-		pop.style.top = wordPos_y + "px"
-		pop.style.left = wordPos_x + "px"
 
-		document.getElementsByTagName("body")[0].appendChild(pop)
-	}
+
+  // 翻译主体
+  let symbol_dom = document.createElement('div');
+  symbol_dom.className = 'translate';
+
+
+  if (result.basic) {
+    // 音标
+    let symbol_ph = document.createElement('div');
+    symbol_ph.className = 'translate_ph';
+    const phs = {
+      phonetic: '标',
+      'us-phonetic': '美',
+      'uk-phonetic': '英'
+    };
+    const phText = [];
+    Object.keys(phs).map(key => {
+      if (!result.basic[key]) {
+        return;
+      }
+      phText.push(`${phs[key]}: /${result.basic[key]}/`);
+    })
+    symbol_ph.innerHTML = phText.join(' ❥➻ ');
+    symbol_dom.appendChild(symbol_ph);
+
+    // 解释
+    if (result.basic.explains) {
+      let tsBasic = document.createElement('div');
+      tsBasic.className = 'translate_basic';
+      result.basic.explains.map(exp => {
+        let part = document.createElement('div');
+        part.className = 'translate_basic_parts';
+
+        let aj = exp.split('.');
+        if (aj[1]) { // 英文
+          let means = aj[1].split('；').map(mean => {
+            return `<li>${mean}</li>`
+          })
+          part.innerHTML = `${aj[0]}<ol>${means.join('')}</ol>`;
+        } else {  // 中文
+          let means = `<li>${exp}</li>`;
+          part.innerHTML = `${means}`;
+        }
+
+        tsBasic.appendChild(part);
+      })
+      symbol_dom.appendChild(tsBasic);
+    }
+  }
+
+  // web解释
+  let tsWeb = document.createElement('div');
+  tsWeb.classList = (result.basic && result.basic.explains) ? 'translate_web dashed' : 'translate_web';
+  if (result.web) {
+    result.web.map(symbol => {
+      let part = document.createElement('div');
+      part.className = 'translate_web_parts';
+      part.innerHTML = `<h5>${symbol.key}:</h5>
+          <p>${symbol.value.join(', ')}</p>`;
+      tsWeb.appendChild(part);
+    });
+  }
+
+  symbol_dom.appendChild(tsWeb);
+  pop.appendChild(symbol_dom);
+  document.getElementsByTagName('body')[0].appendChild(pop);
 }
